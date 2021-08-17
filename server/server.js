134a1943dd45069b2c79873a4085e18016076940
nisozakuto <nisozakuto@gmail.com>
2021-08-16 21:25:58 -0400

    App.post

--- server/server.js
+++ server/server.js
@@ -13,7 +13,19 @@ const storeItems = new Map([
     [2, { priceInCents: 2000, name: "Learn CsS" }]
 ])
 
-app.post('/create-checkout-session', (req, res) => {
+app.post('/create-checkout-session', async (req, res) => {
+    try {
+        const sesion = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            mode: 'payment',
+            success_url: `${process.env.SERVER_URL}/success.html`,
+            cancel_url: `${process.env.SERVER_URL}/cancel.html`,
+        })
+        res.json({ url: sesion.url })
+    }
+    catch (e) {
+        res.status(500).json({ error: e.message })
+    }
     res.json({ url: "hi" })
 })
 
