ebc3b017a2292fd27474894f5f26cabe5f57ff25
nisozakuto <nisozakuto@gmail.com>
2021-08-16 21:02:50 -0400

    express

---
+++ server/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config
+
+const express = require('express')
+const app = express
+
+app.use(express.json())
+
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
+
+const storeItems = new Map([
+    [1, { priceInCents: 1000, name: "Learn react" }],
+    [2, { priceInCents: 2000, name: "Learn CsS" }]
+])
+
+app.listen(3000)
